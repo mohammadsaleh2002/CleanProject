9cdb0a78052074586a22f016373ff64c459441b6
mohammad <mohammad@gmail.com>
2025-11-13 15:55:55 +0330

    Add AppDbContext

---
+++ MyStore/MyStore.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Entities;
+using System.Reflection;
+
+namespace MyStore.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define the tables (DbSets)
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // This line scans the entire assembly (project) for any configuration classes.
+            // We will use this later if we add complex configurations.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Example of setting decimal precision manually (optional but good practice)
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
--- MyStore/MyStore.Infrastructure/obj/Debug/net8.0/MyStore.Infrastructure.AssemblyInfo.cs
+++ MyStore/MyStore.Infrastructure/obj/Debug/net8.0/MyStore.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7d04cfcc198c6f7e6cfa489745cf0b5edebaa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c9063d3b79e734bb531afbcb38ad47f9d008f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
