b22b59131f1ad4cc307c2c4270334d86cc0f51a5
mohammad <mohammad@gmail.com>
2025-11-14 15:22:59 +0330

    implement IUserService

---
+++ MyStore/MyStore.Application/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using MyStore.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace MyStore.Application.Interfaces
+{
+    // Interface for Manual Authentication Logic
+    public interface IUserService
+    {
+        // Registration: Creates the user and hashes the password
+        Task<bool> RegisterUserAsync(User user, string password);
+
+        // Login: Verifies the password and returns the User object if successful
+        Task<User?> AuthenticateUserAsync(string email, string password);
+
+        // Utility: Find user by ID/Email
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByIdAsync(int id);
+    }
+}
